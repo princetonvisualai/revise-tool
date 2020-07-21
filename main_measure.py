817c80881e6bb14a12c18f1ba864decc82600860
Angelina-Wang <angelina.wang@berkeley.edu>
2020-07-21 15:39:45 -0400

    Update main_measure.py

--- main_measure.py
+++ main_measure.py
@@ -71,6 +71,11 @@ def main():
         print("Starting measurement {}".format(meas))
         if args.dataset == 'yfcc':
             dataset = YfccPlacesDataset(transform_train, meas)
+            dataloader = data.DataLoader(dataset=dataset, 
+              num_workers=0,
+              batch_size=1,
+              collate_fn=collate_fn,
+              shuffle=True)
         index_to_measurement[meas](dataloader, args)
         print("Finished measurement {}".format(meas))
 
